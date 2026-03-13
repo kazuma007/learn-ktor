f3b4032053f72db2e82281850ca87de9e800c04d
Kazuma Morishita <x.kazu@icloud.com>
2026-03-13 19:28:47 +0100

    Use Koin for DI (#2)

--- server/src/main/kotlin/com/visualdiffserver/Application.kt
+++ server/src/main/kotlin/com/visualdiffserver/Application.kt
@@ -1,40 +1,58 @@
 package com.visualdiffserver
 
+import com.visualdiffserver.app.productionModule
+import com.visualdiffserver.config.AppConfig
 import com.visualdiffserver.http.configureRouting
-import com.visualdiffserver.persistence.DatabaseFactory
 import com.visualdiffserver.http.plugins.configureSerialization
 import com.visualdiffserver.http.plugins.configureStatusPages
-import com.visualdiffserver.app.AppDependencies
+import com.visualdiffserver.persistence.DatabaseFactory
+import com.visualdiffserver.persistence.DiffRepository
+import com.visualdiffserver.storage.StorageService
 import com.visualdiffserver.worker.RunWorker
+import com.visualdiffserver.worker.VisualDiffRunner
 import io.ktor.server.application.Application
 import io.ktor.server.application.ApplicationStopped
+import io.ktor.server.application.install
 import io.ktor.server.application.log
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import org.koin.core.module.Module
+import org.koin.ktor.ext.inject
+import org.koin.ktor.plugin.Koin
 
 fun main() {
     val port = System.getenv("PORT")?.toIntOrNull() ?: 8080
     embeddedServer(Netty, port = port, module = Application::module).start(wait = true)
 }
 
-fun Application.module(dependencies: AppDependencies = AppDependencies.fromEnv()) {
+fun Application.module(
+    initializeDatabase: Boolean = true,
+    rootModule: Module = productionModule(),
+    extraModules: List<Module> = emptyList(),
+) {
+    install(Koin) {
+        modules(listOf(rootModule) + extraModules)
+    }
+
     configureSerialization()
     configureStatusPages()
+    configureRouting()
 
-    if (dependencies.initializeDatabase) {
-        DatabaseFactory.init(dependencies.config)
+    val config by inject<AppConfig>()
+    val storage by inject<StorageService>()
+    val repository by inject<DiffRepository>()
+    val visualDiffRunner by inject<VisualDiffRunner>()
+
+    if (initializeDatabase) {
+        DatabaseFactory.init(config)
     }
-    configureRouting(
-        repository = dependencies.repository,
-        storage = dependencies.storage,
-    )
 
     val worker = RunWorker(
         logger = log,
-        config = dependencies.config,
-        storage = dependencies.storage,
-        repository = dependencies.repository,
-        visualDiffRunner = dependencies.visualDiffRunner,
+        config = config,
+        storage = storage,
+        repository = repository,
+        visualDiffRunner = visualDiffRunner,
     )
     worker.start()
 
---
+++ server/src/main/kotlin/com/visualdiffserver/app/KoinModules.kt
@@ -0,0 +1,17 @@
+package com.visualdiffserver.app
+
+import com.visualdiffserver.config.AppConfig
+import com.visualdiffserver.persistence.DiffRepository
+import com.visualdiffserver.persistence.ExposedDiffRepository
+import com.visualdiffserver.storage.StorageService
+import com.visualdiffserver.worker.ShellVisualDiffRunner
+import com.visualdiffserver.worker.VisualDiffRunner
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+fun productionModule(config: AppConfig = AppConfig.fromEnv()): Module = module {
+    single { config }
+    single { StorageService(get()) }
+    single<DiffRepository> { ExposedDiffRepository() }
+    single<VisualDiffRunner> { ShellVisualDiffRunner() }
+}
